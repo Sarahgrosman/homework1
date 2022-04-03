04bec0460c6628b3de9111e27ebb5e71f2425e0c
Sarahgrosman <sarahg@webschool.co.il>
2022-04-03 11:16:07 +0300

    עדכון

--- first-node-project/index.js
+++ first-node-project/index.js
@@ -4,10 +4,22 @@ console.log(user)
 const app = express();
 const PORT = 5001
 app.get('/index',(req,res)=>{
-console.log(res);
+
  res.send(user.index)
  
 
 })
+
+app.get('/eng',(req,res)=>{
+ res.send(user.eng)
+})
+
+app.get('/heb',(req,res)=>{
+    res.send(user.heb)
+   })
+
+   app.get('/goodBye',(req,res)=>{
+    res.send(user.gb)
+   })
 
 app.listen(PORT)
